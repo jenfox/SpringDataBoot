f145f701601e41bd25facf7c3476852f70f782df
unknown <jrufox@gmail.com>
2018-03-26 15:16:50 -0400

    uncomment security

--- Project2Alpha/src/main/java/com/revature/projecttwo/security/WebSecurityConfig.java
+++ Project2Alpha/src/main/java/com/revature/projecttwo/security/WebSecurityConfig.java
@@ -1,39 +1,28 @@
-// TODO uncomment in POM
-// package com.revature.projecttwo.security;
-//
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import
-// org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import
-// org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import
-// org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-// import org.springframework.security.core.userdetails.User;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//
-/// **
-// */
-// @Configuration
-// @EnableWebSecurity
-// public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-// @Override
-// protected void configure(HttpSecurity http) throws Exception {
-// http.authorizeRequests().antMatchers("/**").permitAll();
-// // http.authorizeRequests().antMatchers("/",
-// // "/home").permitAll().anyRequest().authenticated().and().formLogin()
-// // .loginPage("/login").permitAll().and().logout().permitAll();
-// }
-//
-// @Bean
-// @Override
-// public UserDetailsService userDetailsService() {
-// UserDetails user =
-// User.withDefaultPasswordEncoder().username("user").password("password").roles("USER")
-// .build();
-//
-// return new InMemoryUserDetailsManager(user);
-// }
-// }
+package com.revature.projecttwo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/**").permitAll();
+		// http.authorizeRequests().antMatchers("/",
+		// "/home").permitAll().anyRequest().authenticated().and().formLogin()
+		// .loginPage("/login").permitAll().and().logout().permitAll();
+	}
+
+	// @Bean
+	// @Override
+	// public UserDetailsService userDetailsService() {
+	// UserDetails user =
+	// User.withDefaultPasswordEncoder().username("user").password("password").roles("USER")
+	// .build();
+	//
+	// return new InMemoryUserDetailsManager(user);
+	// }
+}
