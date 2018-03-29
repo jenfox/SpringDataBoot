9dfd74d35964e6f1c68b1f110ee37f800b2e7afc
unknown <jrufox@gmail.com>
2018-03-28 21:14:55 -0400

    slvf logging impl

--- Project2Alpha/src/main/java/com/revature/Project2AlphaApplication.java
+++ Project2Alpha/src/main/java/com/revature/Project2AlphaApplication.java
@@ -1,12 +1,16 @@
 package com.revature;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Project2AlphaApplication {
+	private static final Logger logger = LoggerFactory.getLogger(Project2AlphaApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("Started App");
 		SpringApplication.run(Project2AlphaApplication.class, args);
 	}
 }
